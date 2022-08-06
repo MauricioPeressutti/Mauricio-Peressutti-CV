5873b6a381eeb760a78ff52c797d203736d664b7
MauricioPeressutti <82254015+MauricioPeressutti@users.noreply.github.com>
2022-08-06 16:21:25 -0300

    fix 4

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ThemePalette } from '@angular/material/core';
+import { ProgressSpinnerMode } from '@angular/material/progress-spinner';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
-
   ngOnInit(): void {
+  }
+
+  constructor(
+    private router:Router
+  ) { }
+
+  color: ThemePalette = 'primary';
+  mode: ProgressSpinnerMode = 'determinate';
+  value = 80;
+  value1 = 65;
+  value2 = 50;
+  title = 'CV Mauricio Peressutti';
+
+  goToAngular(){
+    // this.router. ('https://angular.io/')
+  }
+  goToLN(){
+    this.router.navigateByUrl('')
+  }
+  goToIG(){
+    this.router.navigateByUrl('')
   }
 
 }
